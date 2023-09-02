9d0aff7f86a94a130a07d0dfc264ea529c11549e
Николай <ibraevnikolay@mail.ru>
2023-09-02 18:46:34 +0500

    вторая промежуточная работа на java 2

---
+++ secondTest/Main.java
@@ -0,0 +1,11 @@
+package org.example.secondTest;
+
+import org.example.secondTest.view.View;
+import org.example.sem_3.homework.modul.NoFolder;
+
+public class Main {
+    public static void main(String[] args) throws NoFolder {
+        View view = new View();
+        view.start();
+    }
+}
