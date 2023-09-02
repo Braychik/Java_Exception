17909e77377cb26dece43ca0621bfde13b8bf938
Николай <ibraevnikolay@mail.ru>
2023-09-02 18:43:23 +0500

    вторая промежуточная работа на java

---
+++ secondTest/controllers/Controller2.java
@@ -0,0 +1,8 @@
+package org.example.secondTest.controllers;
+
+import java.util.List;
+
+public interface Controller2 {
+    String get();
+    List<String> getToyQueue();
+}
---
+++ secondTest/moduls/NoFolder.java
@@ -0,0 +1,9 @@
+package org.example.secondTest.moduls;
+
+import java.io.IOException;
+
+public class NoFolder extends IOException {
+    public NoFolder(){
+        super("Папки 'data' небыло, я ее уже создал, повторите операцию.");
+    }
+}
